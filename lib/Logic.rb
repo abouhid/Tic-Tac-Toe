fa945d87a237e76b1973a9f806c8a3afe52301d8
abouhid <alexandre.bouhid@engenharia.ufjf.br>
2020-08-10 17:08:58 -0300

    Create Players.rb

--- lib/Logic.rb
+++ lib/Logic.rb
@@ -1,10 +1,12 @@
+require_relative '../lib/Players.rb'
+
 class Logic
   attr_reader :board
   WINNING_MOVES = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]].freeze
   def initialize
     @board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    @player1_array = []
-    @player2_array = []
+    @player1_array = Players.player1_array
+    @player2_array = Players.player2_array
   end
 
   def update(move, current_player)
